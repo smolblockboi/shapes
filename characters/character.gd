62aa5d0336da78ad2739f79b4a98ea0dd3f6d46c
smolblockboi <tidus1872@hotmail.com>
2023-11-01 20:50:47 -0700

    Character WIP

--- characters/character.gd
+++ characters/character.gd
@@ -1,14 +1,22 @@
 extends CharacterBody3D
 class_name Character
 
+
+@onready var body = $Body
+
 var input_dir : Vector2 = Vector2.ZERO
 var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity") * 0.5
 
 @export var move_speed : float = 200.0
 @export var jump_power : float = gravity * 1.5
 
+var camera_ref : Camera3D
+
 
 func _physics_process(delta):
+	if camera_ref:
+		body.rotation_degrees = camera_ref.get_parent().rotation_degrees
+	
 	get_inputs(delta)
 	
 	move_and_slide()
